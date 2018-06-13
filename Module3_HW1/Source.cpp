a82717b77784211ea380dd019d7da51d44dc6930
айкеновад <@229_13>
2018-06-13 10:32:42 +0600

    -

--- Module3_HW1/Source.cpp
+++ Module3_HW1/Source.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
 #include<math.h>
@@ -17,7 +17,6 @@ void main()
 	int tn = 0;
 
 start:
-
 	printf("task number:\t");
 	scanf("%d", &tn);
 
